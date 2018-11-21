139f1c36c48624d5c09880c3b69904ea1e20ad80
Lucas Misael <lucas.ms.elo84@gmail.com>
2018-11-20 23:31:36 -0200

    Update BD/newpsicosys.sql

--- BD/newpsicosys.sql
+++ BD/newpsicosys.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 20-Nov-2018 às 00:54
--- Versão do servidor: 10.1.25-MariaDB
--- PHP Version: 7.1.7
+-- Generation Time: 21-Nov-2018 às 02:30
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -31,7 +31,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `tab_clientes` (
   `CLI_COD` int(11) NOT NULL,
   `CLI_NOME` varchar(100) DEFAULT NULL,
-  `CLI_STATUS` int(11) NOT NULL,
+  `CLI_STATUS` varchar(3) DEFAULT NULL,
   `CLI_CPF` varchar(50) NOT NULL,
   `CLI_DATA_NASC` date NOT NULL,
   `CLI_ENDERECO` varchar(200) DEFAULT NULL,
@@ -60,7 +60,8 @@ CREATE TABLE `tab_clientes` (
 --
 
 INSERT INTO `tab_clientes` (`CLI_COD`, `CLI_NOME`, `CLI_STATUS`, `CLI_CPF`, `CLI_DATA_NASC`, `CLI_ENDERECO`, `CLI_CEP`, `CLI_CONTATO_FONE1`, `CLI_FONE1`, `CLI_CONTATO_CEL1`, `CLI_CEL1`, `CLI_CONTATO_FONE2`, `CLI_FONE2`, `CLI_CONTATO_CEL2`, `CLI_CEL2`, `CLI_EMAIL`, `CLI_COD_CID`, `CLI_LIBERACAO`, `CLI_CONVENIO`, `CLI_OBS`, `CLI_TIPO`, `CLI_RESP`, `CLI_DATA_CADASTRO`, `CLI_PERIODO`) VALUES
-(1, 'Maria Helena Nogueira Ferreira Oliveira', 0, '295.664.168-90', '1980-09-04', 'Rua Pedro Gusso', '81.050-200', 'Maria Helena', '(41) 3079-5449', 'Miguel', '(41) 98864-0013', 'Tiago', '(41) 3276-6487', 'Tiago', '(41) 98805-5870', 'lenaenf@hotmail.com', '102030405060708090', 'JPK3080', 1, 'observacaaooooooooooooooooooooooooooooooooooo', 'Crianca', 'TiagÃ£o das Meninas', '0000-00-00', 'Manha');
+(1, 'Maria Helena Nogueira Ferreira Oliveira', 'on', '295.664.168-90', '1980-09-04', 'Rua Pedro Gusso', '81.050-200', 'Maria Helena', '(41) 3079-5449', 'Miguel', '(41) 98864-0013', 'Tiago', '(41) 3276-6487', 'Tiago', '(41) 98805-5870', 'lenaenf@hotmail.com', '102030405060708090', 'JPK3080', 1, 'observacaaooooooooooooooooooooooooooooooooooo', 'Crianca', 'TiagÃ£o das Meninas', '0000-00-00', 'Manha'),
+(2, 'Lucas Misael Barbosa do Nascimento', 'off', '098.137.912-14', '1997-07-02', 'Joana roncaglio bertoldi', '81.490-468', '', '', '', '', '', '', '', '', 'lucas.ms.elo84@gmail.com', '1215', '', 1, '', 'Crianca', '', '0000-00-00', 'Manha');
 
 -- --------------------------------------------------------
 
@@ -95,13 +96,37 @@ INSERT INTO `tab_convenios` (`CONV_COD`, `CONV_STATUS`, `CONV_NOME`, `CONV_CNPJ`
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `tab_eventos`
+--
+
+CREATE TABLE `tab_eventos` (
+  `id` int(11) NOT NULL,
+  `title` text,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `prof_id` varchar(50) NOT NULL,
+  `cli_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tab_eventos`
+--
+
+INSERT INTO `tab_eventos` (`id`, `title`, `start`, `end`, `prof_id`, `cli_id`) VALUES
+(73, 'Maria Helena Nogueira Ferreira Oliveira', '2018-11-20 22:00:00', '2018-11-20 23:00:00', '1', '1'),
+(75, 'Maria Helena Nogueira Ferreira Oliveira', '2018-11-20 20:00:00', '2018-11-20 20:30:00', '2', '1'),
+(76, 'Lucas Misael Barbosa do Nascimento', '2018-11-20 22:00:00', '2018-11-20 22:00:00', '2', '1');
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `tab_profissionais`
 --
 
 CREATE TABLE `tab_profissionais` (
   `PROF_COD` int(11) NOT NULL,
   `PROF_NOME` varchar(100) NOT NULL,
-  `PROF_STATUS` int(11) NOT NULL,
+  `PROF_STATUS` varchar(3) NOT NULL,
   `PROF_ESPECIALIDADE` varchar(100) DEFAULT NULL,
   `PROF_TIPO_CRIANCA` varchar(1) DEFAULT NULL,
   `PROF_TIPO_ADOLESCENTE` varchar(1) DEFAULT NULL,
@@ -126,7 +151,8 @@ CREATE TABLE `tab_profissionais` (
 --
 
 INSERT INTO `tab_profissionais` (`PROF_COD`, `PROF_NOME`, `PROF_STATUS`, `PROF_ESPECIALIDADE`, `PROF_TIPO_CRIANCA`, `PROF_TIPO_ADOLESCENTE`, `PROF_TIPO_ADULTO`, `PROF_TIPO_IDOSO`, `PROF_CNPJ_CPF`, `PROF_CONSELHO`, `PROF_DATA_NASC`, `PROF_ENDERECO`, `PROF_CEP`, `PROF_FONE1`, `PROF_CEL1`, `PROF_EMAIL`, `PROF_CONVENIO`, `PROF_OBS`, `PROF_FONE2`, `PROF_CEL2`) VALUES
-(1, 'Tiago Dantas de Oliveira', 0, 'Ginecologista', 'A', NULL, NULL, NULL, '030.471.279-56', 'COREN', '1980-09-26', 'Rua Pedro Gusso, 12 ', '81.050-200', '(41) 3276-6457', '(41) 98805-5870', 'tiago@softdib.com.br', 1, 'Ã© tudo nossoooooooooo', '(41) 3079-5449', '(41) 98864-0013');
+(1, 'Tiago Dantas de Oliveira', 'off', 'Ginecologista', 'A', NULL, NULL, NULL, '030.471.279-56', 'COREN', '1980-09-26', 'Rua Pedro Gusso, 12 ', '81.050-200', '(41) 3276-6457', '(41) 98805-5870', 'tiago@softdib.com.br', 1, 'Ã© tudo nossoooooooooo', '(41) 3079-5449', '(41) 98864-0013'),
+(2, 'Lucas Misael Barbosa do Nascimento', 'off', '', 'A', NULL, NULL, NULL, '', '', '1997-07-02', 'Joana roncaglio bertoldi', '81.490-468', '', '', 'lucas.ms.elo84@gmail.com', 1, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -138,16 +164,18 @@ CREATE TABLE `tab_usuarios` (
   `USU_COD` int(11) NOT NULL,
   `USU_NOME` varchar(100) DEFAULT NULL,
   `USU_EMAIL` varchar(100) NOT NULL,
-  `USU_SENHA` varchar(100) NOT NULL
+  `USU_SENHA` varchar(100) NOT NULL,
+  `Usu_Status` varchar(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `tab_usuarios`
 --
 
-INSERT INTO `tab_usuarios` (`USU_COD`, `USU_NOME`, `USU_EMAIL`, `USU_SENHA`) VALUES
-(1, 'Tiago', 'tiago@softdib.com.br', 'tiago'),
-(2, 'Admin', 'administrador@psicosys.com.br', '1234');
+INSERT INTO `tab_usuarios` (`USU_COD`, `USU_NOME`, `USU_EMAIL`, `USU_SENHA`, `Usu_Status`) VALUES
+(0, 'poderoso', 'god@gmail.com', '1234', 'on'),
+(1, 'Tiago', 'tiago@softdib.com.br', 'tiago', NULL),
+(2, 'Admin', 'administrador@psicosys.com.br', '123', 'on');
 
 --
 -- Indexes for dumped tables
@@ -164,6 +192,12 @@ ALTER TABLE `tab_clientes`
 --
 ALTER TABLE `tab_convenios`
   ADD PRIMARY KEY (`CONV_COD`);
+
+--
+-- Indexes for table `tab_eventos`
+--
+ALTER TABLE `tab_eventos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tab_profissionais`
@@ -185,17 +219,26 @@ ALTER TABLE `tab_usuarios`
 -- AUTO_INCREMENT for table `tab_clientes`
 --
 ALTER TABLE `tab_clientes`
-  MODIFY `CLI_COD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `CLI_COD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `tab_convenios`
 --
 ALTER TABLE `tab_convenios`
   MODIFY `CONV_COD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tab_eventos`
+--
+ALTER TABLE `tab_eventos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
 --
 -- AUTO_INCREMENT for table `tab_profissionais`
 --
 ALTER TABLE `tab_profissionais`
-  MODIFY `PROF_COD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+  MODIFY `PROF_COD` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
