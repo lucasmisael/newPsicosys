b7471c9f6476b282507fbc236595cd348d271a12
Lucas Misael <lucas.ms.elo84@gmail.com>
2018-12-06 17:07:33 -0200

    indicadores

--- header.php
+++ header.php
@@ -48,6 +48,8 @@
             <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.4.1/js/buttons.print.min.js"></script>
             <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
             <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+            <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+            
 
 
 
